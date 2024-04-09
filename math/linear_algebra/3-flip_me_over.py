74a3b474fad1e311c892bf175e034ebba29520d0
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 17:17:23 -0400

    Add a function to transpose a matrix

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Function to transpose a matrix """
+
+
+def matrix_transpose(matrix):
+    # Get the number of rows and columns in the matrix
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Create a new matrix with swapped dimensions
+    transpose = [[0 for _ in range(rows)] for _ in range(cols)]
+
+    # Populate the transpose matrix
+    for i in range(rows):
+        for j in range(cols):
+            transpose[j][i] = matrix[i][j]
+
+    return transpose
