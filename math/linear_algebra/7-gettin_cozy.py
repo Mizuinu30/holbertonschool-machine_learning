e0e2b250abcea0cf6b5cc4a3c19f9de112f75ab0
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 19:41:53 -0400

    Add function that concatanates 2 matrices

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -3,7 +3,7 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    """ """
+    """ Concatenates two matrices along a specific axis"""
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
     if axis == 1 and len(mat1) == len(mat2):
