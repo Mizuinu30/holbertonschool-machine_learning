a40912c4fb2f1b4b977f92db0409a88deadff784
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 16:53:35 -0400

    Add function to calculate a shape of a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,12 +3,14 @@
 
 def matrix_shape(matrix):
     """Calculates the shape of a matrix"""
-    if not isinstance(matrix, list):
-        return []
-    if len(matrix) == 0:
-        return [0]
-    shape = matrix_shape(matrix[0])
-    return [len(matrix)] + shape
+    shape = [] # Initialize the shape of the matrix
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) > 0:
+            matrix = matrix [0]
+        else:
+            break
+    return shape
 
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 print(matrix_shape(matrix))
