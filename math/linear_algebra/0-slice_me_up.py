6ae9892326a29ddebca86e3d8c01e6b362d29b04
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 14:44:12 -0400

    Removed lines 14 onwards

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+#Define an array
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = [9, 8] # your code here
-arr2 =  [9, 4, 1, 0, 3]# your code here
-arr3 =  [8, 2, 3, 9, 4]# your code here
+# Slice the array to include the first two numbers
+arr1 = arr[:2]
+# Slice the array to include the last five numbers
+arr2 = arr[-5:]
+# Slice the array to include the 2nd through 6th numbers
+arr3 = arr[1:6]
+# Print the arrays
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
