331c89b47321fdf39d6893789b5112ad62856e87
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 17:13:02 -0400

    fixed pycode

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Function to calculate the shape of a matrix """
 
+
 def matrix_shape(matrix):
     """Calculates the shape of a matrix"""
-    shape = [] # Initialize the shape of the matrix
+    shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
-        matrix = matrix [0]
+        matrix = matrix[0]
     return shape
