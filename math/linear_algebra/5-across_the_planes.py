90eceea55ec801fc6a9b3a5410e6e8bb6431aa6b
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 18:34:09 -0400

    Add function to add 2 matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,5 +6,5 @@ def add_matrices2D(mat1, mat2):
     """ Adds two 2D matrices element-wise """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]\
-        for x in range(len(mat1))]
+    return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]
+    for x in range(len(mat1))]
