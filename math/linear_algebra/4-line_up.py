a6d64fa0eb8458e971b871599381508993ac1506
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 17:53:17 -0400

    Added line 7 to verify in arrasy are tnot the same

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,6 +4,8 @@
 
 def add_arrays(arr1, arr2):
     """ adds two arrays element-wise """
+    if len(arr1) != len(arr2):
+        return None
     sum_arrays = [x + y for x, y in zip(arr1, arr2)]
  
     return(sum_arrays)
