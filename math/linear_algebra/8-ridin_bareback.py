07cbd725a3521c28b05f8b46759b5936d910ab9b
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-09 20:32:32 -0400

    Add a function that multiplies

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Function that performs matrix multiplication """
+
+
+def mat_mul(mat1, mat2):
+    """ Performs matrix multiplication """
+    if len(mat1[0]) != len(mat2):
+        return None
+    matrix12 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                matrix12[i][j] += mat1[i][k] * mat2[k][j]
+    return matrix12
