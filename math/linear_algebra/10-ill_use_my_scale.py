550037ff8f0ca2277c6317df3fec70dab320708a
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-10 12:14:18 -0400

    Add fucntion to determine the shape of a numpy.ndarray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Function that determines the sahpe of a numpy.ndarray """
+
+
+def np_shape(matrix) -> tuple:
+    """ Returns the shape as a tuple """
+    return matrix.shape
