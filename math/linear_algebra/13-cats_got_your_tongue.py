97cff191bfc6d68f1477e5877fb0fef6a175f9b8
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-11 11:17:56 -0400

    A function that concatenates two arrays

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-""" Concatenates two arrays """
+""" A function that concatenates two arrays """
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """
-    Concatenates two arrays
-    """
+    """Concatenates two arrays"""
     return np.concatenate((mat1, mat2), axis=axis)
