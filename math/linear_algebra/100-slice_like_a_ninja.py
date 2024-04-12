520106bf71ccc5e6dfaa9c56ec9ab697e5f2a9e1
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-12 13:10:10 -0400

    fixed typo on line 7

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -4,7 +4,7 @@
 
 def np_slice(matrix, axes={}):
     """ A function that slices a matrix along specific axes """
-    slices = [slice(None)] * Len(matrix)
+    slices = [slice(None)] * len(matrix)
     for axis, slice_range in axes.items():
         slices[axis] = slice(*slice_range)
     return [row[tuple(slices)] for row in matrix]
