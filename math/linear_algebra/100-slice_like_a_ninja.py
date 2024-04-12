fff3377d081ff6c221b5bec80d1a5cac5ccb7106
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-12 13:07:25 -0400

    Version without importing numpy

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-""" A function that slices a matrix along specific axes"""
+""" Function to slice a matrix along specific axes """
 
-
-import numpy as np
 
 def np_slice(matrix, axes={}):
-    """ Return a sliced matrix along specific axes """
-    slices = [slice(None)] * matrix.ndim
+    """ A function that slices a matrix along specific axes """
+    slices = [slice(None)] * Len(matrix)
     for axis, slice_range in axes.items():
         slices[axis] = slice(*slice_range)
-    return matrix[tuple(slices)]
-        
+    return [row[tuple(slices)] for row in matrix]
