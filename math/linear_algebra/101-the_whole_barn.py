345ad2c2aeb20de1cc1ed9e58b8b7abda8ad05c0
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-14 18:21:06 -0400

    Add A function that adds 2 matrices

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-""" A function that adds 2 matrices"""
-
+""" defines function that adds two matrices """
 
 def matrix_shape(matrix):
-    """returns a list os integers representing the dimensions of a matrix"""
-    matrix_shape = []
-    while type(matrix) is list:
-        matrix_shape.append(len(matrix))
-        matrix = matrix[0]
-    return matrix_shape
+    """ returns list of integers representing dimensions of given matrix """
+    shape = []
+    while isinstance(matrix, list):  # Checks if the input is a list (matrix)
+        shape.append(len(matrix))    # Appends the size of the current dimension
+        matrix = matrix[0]           # Moves to the next dimension
+    return shape
 
 def add_matrices(mat1, mat2):
-    """Sum of matrices"""
-    if matrix_shape(mat1) != matrix_shape(mat2)
+    """ returns new matrix that is sum of two matrices added element-wise """
+    
+    # Check if the dimensions of both matrices are the same
+    if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    if len(matrix_shape(mat1)) is 1:
-        return [mat1[i] + mat2[i] for i in range(len(mat1))]
-    return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
+    
+    # Handle the addition based on the dimensionality of the matrices
+    if isinstance(mat1[0], list):  # If the element is a list, proceed recursively
+        return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
+    else:  # Base case: elements of the matrices are not lists (i.e., actual numbers)
+        return [x + y for x, y in zip(mat1, mat2)]
