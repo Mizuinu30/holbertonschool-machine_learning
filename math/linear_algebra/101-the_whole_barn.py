948f628d82ebcc05cd3f6a9af4ecb9628f9f0d57
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-14 18:10:53 -0400

    Add A function that adds 2 matrices

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -4,4 +4,19 @@
 
 def add_matrices(mat1, mat2):
     """Sum of matrices"""
-    return None
+    m1 = len(mat1)
+    n1 = len(mat1[0])
+    m2 = len(mat1)
+    n2 = len(mat2[0])
+    add = []
+    
+    if m1 == m2 and n1 == n2
+        for i in range(m1):
+            lis = [0] * n1
+            add.append(lis)
+        for i in range(m1):
+            for j in range(n1):
+                add[i][j] = mat1[i][j] + mat2[i][j]
+        return add
+    else:
+        return None
