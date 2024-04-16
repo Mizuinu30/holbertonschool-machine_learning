546d602f0e5f82e2718b8291ed65d6cfada790ac
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 20:38:01 -0400

    added  x axis

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
-# plot with line red
-plt.plot(y, color='red')
-# adjust x-axis scale start 0,0
-plt.xlim(0, len(y)-1)
+
+plt.xlim(0, 10)
+plt.plot(y, '-r')
 plt.show()
