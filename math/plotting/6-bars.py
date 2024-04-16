d40f9df4f6a43c668fc513281a52ca0744f8651a
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-16 10:42:07 -0400

    Add a function that creates a plot for bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
-""" Function that creates a plot for bars"""
-
+""" plot a stacked bar chart for the amount and types of fruit people have """
 import numpy as np
 import matplotlib.pyplot as plt
 
-def bars():
-    np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4, 3))
-    plt.figure(figsize=(6.4, 4.8))
-    fruit_names = ['Apples', 'Bananas', 'Oranges', 'Peaches']
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-    people = ['Farrah', 'Fred', 'Felicia']
-    bar_positions = np.arange(len(people))
-    bottoms = np.zeros(len(people))
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4, 3))
 
-    for i, (name, color) in enumerate(zip(fruit_names, colors)):
-        plt.bar(bar_positions, fruit[i, :], color=color, width=0.5, bottom=bottoms, label=name)
-        bottoms += fruit[i, :]
+people = ['Farrah', 'Fred', 'Felicia']
+fruit_names = {
+    'apples': 'red',
+    'bananas': 'yellow',
+    'oranges': '#ff8000',
+    'peaches': '#ffe5b4'
+}
 
-    plt.xticks(bar_positions, people)
-    plt.ylabel('Quantity of Fruit')
-    plt.title('Number of Fruit per Person')
-    plt.ylim(0, 80)
-    plt.yticks(np.arange(0, 81, 10))
-    plt.legend()
-    plt.show()
-
-if __name__ == '__main__':
-    bars()
+i = 0
+for name, color in sorted(fruit_names.items()):
+    bottom = 0
+    for j in range(i):
+        bottom += fruit[j]
+    plt.bar(
+        np.arange(len(people)),
+        fruit[i],
+        width=0.5,
+        bottom=bottom,
+        color=color,
+        label=name)
+    i += 1
+plt.xticks(np.arange(len(people)), people)
+plt.yticks(np.arange(0, 81, 10))
+plt.ylabel('Quantity of Fruit')
+plt.title("Number of Fruit per Person")
+plt.legend()
+plt.show()
