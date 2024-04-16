4fa6118d6728f182a7aba2b2bc774a1d9c0fe1d8
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-16 09:40:59 -0400

    hot fix

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""A function that creates a scatter plot"""
-
-
+"""Module that creates a scatter plot"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
-    """ Crestes a scatter plot"""
+    """Function that creates a scatter plot of the data"""
+    
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -14,10 +14,12 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, color="magenta")
+    plt.scatter(x, y, color='magenta')
+
     plt.xlabel('Height (in)')
-    plt.ylabel('weight (lbs)')
+    plt.ylabel('Weight (lbs)')
+
     plt.title("Men's Height vs Weight")
-    
+
     plt.show()
-   
+    
