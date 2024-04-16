89a35ed006df72b6c2e114c7c433fc8c28f9ccf7
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 21:28:55 -0400

    erased line 23

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""A function that creates a scatter plot"""
-
-
+"""Module that creates a scatter plot"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
-    """ Crestes a scatter plot"""
+    """
+    Function that creates a scatter plot of the data
+    """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -14,14 +15,11 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.figure(figsize=(8, 6))  # Adjusting figure size to be more suitable for comparison
-    plt.scatter(x, y, color='magenta', s=10)  # s is the size of the points, set to a standard value for visibility
-    plt.title("Men's Height vs Weight")  # Title of the plot
-    plt.xlabel('Height (in)')  # x-axis label
-    plt.ylabel('Weight (lbs)')  # y-axis label
-    plt.xlim(30, 110)  # Setting x limits to show all data points clearly
-    plt.ylim(100, 260)  # Setting y limits to show all data points clearly
+    plt.scatter(x, y, color='magenta')
+
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+
+    plt.title("Men's Height vs Weight")
+
     plt.show()
-    
-scatter()
-   
