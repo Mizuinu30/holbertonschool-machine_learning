1fa643f1b7e2835b81c7462697f0d3cb152374b6
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 21:49:40 -0400

    Function that crates a histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
+""" A Function that crates a histogram"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def frequency():
-
+    """Function that crates a histogram"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    
+    bin_size = 10
+    bins = np.arange(0, 101, bin_size)
+    
+    plt.figure(figsize=(6.4, 4.8))
+    
+    # Use blue color for bars and align them to the left of bin edges
+    plt.hist(student_grades, bins=bins, color='blue', edgecolor='black', align='left')
 
     plt.xlabel('Grades')
-    plt.ylabel('Number of students')
+    plt.ylabel('Number of Students')
     plt.title('Project A')
     
-    plt.xticks(range(0, 101, 10))
+    plt.xticks(bins)
+    
+    # Remove yticks if they are not required by the reference image
+    # plt.yticks([])
 
+    plt.xlim([0, 100])  # Ensure the x-axis starts at 0 and ends at 100
+    
     plt.show()
 
 frequency()
