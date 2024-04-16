d5b7003835564b4568d04a410129f0dd04ee350c
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 22:12:09 -0400

    add a function to creates a histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-""" Frequency of student grades """
+"""Module that creates a plot for frequency"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def frequency():
-    """ Frequency of student grades"""
+    """
+    Function that creates a plot for frequency
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
+
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+
+    plt.title('Project A')
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
     plt.xlim(0, 100)
     plt.ylim(0, 30)
-    plt.hist(student_grades, bins=10, range=(0, 100), rwidth=10,
-             edgecolor='black')
+    
     plt.show()
-
-frequency()
