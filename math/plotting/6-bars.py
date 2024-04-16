471f84a6d05157041019457cde2a3b86d21e20d8
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-16 18:38:22 -0400

    Add a function that creates a plot for bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -10,32 +10,26 @@ def bars():
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.bar([1, 2, 3], fruit[0], color="red", label="apples", width=0.5)
-    plt.bar(
-        [1, 2, 3], fruit[1], color="yellow", bottom=fruit[0],
-        label="bananas", width=0.5
-    )
-    plt.bar(
-        [1, 2, 3],
-        fruit[2],
-        color="#ff8000",
-        bottom=fruit[0] + fruit[1],
-        label="oranges",
-        width=0.5,
-    )
-    plt.bar(
-        [1, 2, 3],
-        fruit[3],
-        color="#ffe5b4",
-        bottom=fruit[0] + fruit[1] + fruit[2],
-        label="peaches",
-        width=0.5,
-    )
+    names = ["Farrah", "Fred", "Felicia"]
+    colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
+    labels = ["apples", "bananas", "oranges", "peaches"]
+
+    for i in range(4):
+        plt.bar(
+            [1, 2, 3],
+            fruit[i],
+            color=colors[i],
+            bottom=np.sum(fruit[:i], axis=0),
+            label=labels[i],
+            width=0.5,
+        )
+
     plt.ylabel("Quantity of Fruit")
     plt.ylim(0, 80)
     plt.title("Number of Fruit per Person")
-
-    plt.xticks([1, 2, 3], ["Farrah", "Fred", "Felicia"])
+    plt.xticks([1, 2, 3], names)
     plt.legend()
     plt.show()
-    
+
+
+bars()
