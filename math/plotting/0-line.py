00cfa9e3746e36db26e2d3887d3abe7495bb63f4
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 20:49:18 -0400

    added line()

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,10 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
-x = np.arange(0, 11)  # Define x values from 0 to 10
+def line():
+    y = np.arange(0, 11) ** 3
+    x = np.arange(0, 11)  # x values from 0 to 10
 
-plt.figure()          # Start a new figure
-plt.plot(x, y, '-r')  # Plot y against x with a red solid line
-plt.xlim(0, 10)       # Set x-axis limits
-plt.show()            # Show the plot
+    plt.figure()           # Start a new figure
+    plt.plot(x, y, '-r')   # Plot y against x with a red solid line
+    plt.xlim(0, 10)        # Set x-axis limits from 0 to 10
+    plt.show()             # Display the plot
+
+if __name__ == "__main__":
+    line()                 # Call the line function if the script is run directly
