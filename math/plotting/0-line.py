d883a5e5ebd59921e31e4c48575f108baee8c38d
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 20:47:49 -0400

    added  x axis

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
+x = np.arange(0, 11)  # Define x values from 0 to 10
 
-plt.xlim(0, 10)
-plt.plot(y, '-r')
-plt.show()
+plt.figure()          # Start a new figure
+plt.plot(x, y, '-r')  # Plot y against x with a red solid line
+plt.xlim(0, 10)       # Set x-axis limits
+plt.show()            # Show the plot
