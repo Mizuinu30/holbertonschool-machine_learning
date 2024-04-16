0ebe12b7e4e0ee283fefaa4430ce1c63c7c1e1cb
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 21:23:28 -0400

    erased line 23

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -14,7 +14,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, color="magenta")
+    plt.scatter(x, y, color="magenta", s=10)
     plt.xlabel('Height (in)')
     plt.ylabel('weight (lbs)')
     plt.title("Men's Height vs Weight")
