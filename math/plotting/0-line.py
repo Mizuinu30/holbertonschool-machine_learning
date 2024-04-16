546ed74355db1cb4944dfae23a8ca0e984b749eb
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-15 20:56:09 -0400

    fixed pycode

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""A function that plots a red line"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """A function that plots a red line"""
     y = np.arange(0, 11) ** 3
     x = np.arange(0, 11)  # x values from 0 to 10
 
@@ -11,5 +15,6 @@ def line():
     plt.xlim(0, 10)        # Set x-axis limits from 0 to 10
     plt.show()             # Display the plot
 
+
 if __name__ == "__main__":
-    line()                 # Call the line function if the script is run directly
+    line()                 
