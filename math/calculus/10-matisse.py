6ddfb3d26386fb1f0cc61279f30c6c950103a127
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-19 21:45:35 -0400

    Add a Function that calculates the derivative of a polynomial

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""matisse module"""
+
+
+def poly_derivative(poly):
+    """Function that calculates the derivative of a polynomial."""
+    if not isinstance(poly, list) or not all(isinstance(coef, (int, float))
+                                             for coef in poly):
+        return None
+
+    if len(poly) <=1:
+        return [0]
+
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+
+    if not derivative:
+        return [0]
+
+    return derivative
