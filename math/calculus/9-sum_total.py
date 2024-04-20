7593eb6f1aeb8fdd2979656fc82c2b3152ea69f5
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-19 21:33:40 -0400

    fixed pycode

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,7 +3,8 @@
 
 
 def summation_i_squared(n):
-    """ Function that calculates the sum of the squares of the first n integers"""
+    """ Function that calculates the sum of the squares 
+    of the first n integers"""
     if not isinstance(n, int) or n < 1:
         return None
     return n * (n + 1) * (2 * n + 1) // 6
