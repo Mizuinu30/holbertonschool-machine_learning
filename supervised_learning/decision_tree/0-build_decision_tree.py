d6fd16d164a767c11450386d6270fa56fb0217fa
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-24 16:50:54 -0400

    Modified th depth function

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -3,7 +3,9 @@
 
 
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    """ Node of the decision tree"""
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -26,3 +28,34 @@ class Node:
             local_max = max(local_max, right_depth)
 
         return local_max
+
+class Leaf(Node):
+    """ Leaf node of the decision tree"""
+    def __init__(self, value, depth=None):
+        """ Constructor"""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self) :
+        return self.depth
+
+class Decision_Tree():
+    """ Class that builds a decision tree"""
+    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self) :
+        """ Calculate the depth of the tree"""
+        return self.root.max_depth_below()
