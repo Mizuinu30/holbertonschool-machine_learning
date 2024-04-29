3af625ddbb6db6a4df0fd31e4eb388bb9f278026
Mizuinu30 <juansilva.dvm@gmail.com>
2024-04-28 21:22:08 -0400

    Implementation of the class IRF

--- supervised_learning/decision_tree/11-isolation_forest.py
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -10,10 +10,34 @@ Isolation_Random_Tree = __import__('10-isolation_tree').Isolation_Random_Tree
 
 
 class Isolation_Random_Forest():
-    """ This class defines an Isolation Random Forest. """
+    """
+    A class that implements an Isolation Forest for anomaly detection.
+    An Isolation Forest consists of multiple Isolation Random Trees which
+    isolate observations by randomly selecting a feature and then randomly
+    selecting a split value between the maximum and minimum values of the
+    selected feature.
 
+    Attributes:
+        n_trees (int): Number of trees in the forest.
+        max_depth (int): The maximum depth of each tree in the forest.
+        min_pop (int): Minimum sample size in nodes below which a tree
+        will not attempt to split.
+        seed (int): Random seed used for reproducible results.
+        numpy_preds (list): List of prediction functions from each tree in
+        the forest.
+    """
     def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
-        """ Initializes the Isolation Random Forest. """
+        """
+        Initializes the Isolation Random Forest with specified parameters.
+
+        Args:
+            n_trees (int): Number of trees in the forest.
+            max_depth (int): Maximum depth of each tree.
+            min_pop (int): Minimum population size in a node to consider
+            for further splitting.
+            seed (int): Seed for the random number generator to ensure
+            reproducibility.
+        """
         self.numpy_predicts = []
         self.target = None
         self.numpy_preds = None
@@ -22,12 +46,34 @@ class Isolation_Random_Forest():
         self.seed = seed
 
     def predict(self, explanatory):
-        """ Makes predictions for a given set of examples. """
+        """
+        Predicts the anomaly scores for each sample in the dataset.
+
+        Args:
+            explanatory (numpy.ndarray): Data to predict the anomaly
+            scores for.
+
+        Returns:
+            numpy.ndarray: Averaged depth of each sample across all trees
+            in the forest.
+        """
         predictions = np.array([f(explanatory) for f in self.numpy_preds])
         return predictions.mean(axis=0)
 
     def fit(self, explanatory, n_trees=100, verbose=0):
-        """ Fits the model to the training data. """
+        """
+        Fits the Isolation Forest model to the provided data.
+
+        Args:
+            explanatory (numpy.ndarray): The dataset to fit the model.
+            n_trees (int, optional): Number of trees to generate in the
+            forest.
+            verbose (int, optional): Verbosity level; 0 is silent,
+            1 prints the tree statistics after training.
+
+        Returns:
+            None
+        """
         self.explanatory = explanatory
         self.numpy_preds = []
         depths = []
@@ -43,12 +89,26 @@ class Isolation_Random_Forest():
             leaves.append(T.count_nodes(only_leaves=True))
         if verbose == 1:
             print(f"""  Training finished.
-    - Mean depth                     : {np.array(depths).mean()}
-    - Mean number of nodes           : {np.array(nodes).mean()}
-    - Mean number of leaves          : {np.array(leaves).mean()}""")
+    - Mean depth                     : { np.array(depths).mean()      }
+    - Mean number of nodes           : { np.array(nodes).mean()       }
+    - Mean number of leaves          : { np.array(leaves).mean()      }""")
 
     def suspects(self, explanatory, n_suspects):
-        """ Returns the top n_suspects with the smallest depths. """
+        """
+        Identifies the top n suspects with the lowest mean depth in the
+        Isolation Forest, suggesting they are potential outliers.
+
+        Args:
+            explanatory (numpy.ndarray): The explanatory variables of the
+            dataset.
+            n_suspects (int): The number of suspect data points to return.
+
+        Returns:
+            tuple: Two numpy arrays; the first contains the suspect data
+            points,
+                the second contains the corresponding depths indicating
+                their isolation levels.
+        """
         # Calculate the mean depth for each data point using predict method
         depths = self.predict(explanatory)
         # Get the indices that would sort the depths array in ascending order
