fe0e5f707a6f83242286173aeac69f19781c67e1
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-05 11:35:01 -0400

    made file executionable

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,10 +12,8 @@ class Neuron:
         """ Class constructor """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
+        self.W = np.random.randn(1, nx)
         self.b = 0
-        self.w = np.random.normal(size=(1, nx))
         self.A = 0
