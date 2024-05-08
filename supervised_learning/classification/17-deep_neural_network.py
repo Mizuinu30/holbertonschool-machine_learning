42c53bd6d1d36ce5b24daa6e4db8d5a28129d7bd
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-08 13:34:36 -0400

    Add a class DeepNeuralNetwork that defines a deep neural network performing binary classification (based on 16-deep_neural_network.py)

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Module that defines a deep neural network """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class that defines a deep neural network """
+
+    def __init__(self, nx, layers):
+        """
+        Initializes the DeepNeuralNetwork instance
+        Args:
+            nx: is the number of input features
+            layers: is a list representing the number of nodes in each
+                    layer of the network
+        """
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(layers, list) or not layers:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            key_W = "W" + str(i + 1)
+            key_b = "b" + str(i + 1)
+
+            if i == 0:
+                self.__weights[key_W] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights[key_W] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            self.__weights[key_b] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """ Getter function for L """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter function for cache """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter function for weights """
+        return self.__weights
