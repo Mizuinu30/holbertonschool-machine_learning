2ef2d0ef862847e25c69a44a9ee4312dd87e5918
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-08 21:03:51 -0400

    Initial commit

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+""" function def create_placeholders(nx, classes): 
+that returns two placeholders, x and y, for the neural network """
+
+
+import tensorflow as tf
+
+def create_placeholders(nx, classes):
+    """ function def create_placeholders(nx, classes): 
+    that returns two placeholders, x and y, for the neural network """
+    x = tf.compat.v1.placeholder("float", [None, nx], name="x")
+    y = tf.compat.v1.placeholder("float", [None, classes], name="y")
+    return x, y
