ae51aee2854a1eb508139a22dc1339b618c97164
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-08 21:15:23 -0400

    Initial commit

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -3,11 +3,11 @@
 that returns two placeholders, x and y, for the neural network """
 
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 def create_placeholders(nx, classes):
     """ function def create_placeholders(nx, classes): 
     that returns two placeholders, x and y, for the neural network """
-    x = tf.compat.v1.placeholder("float", [None, nx], name="x")
-    y = tf.compat.v1.placeholder("float", [None, classes], name="y")
+    x = tf.placeholder("float", [None, nx], name="x")
+    y = tf.placeholder("float", [None, classes], name="y")
     return x, y
