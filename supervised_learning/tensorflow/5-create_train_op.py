3aee0a1db1b069f57f1c0edcb8682a45b4d1417c
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-12 10:19:27 -0400

    A function that creates the trainig operation for a network

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" function that creates the trainig operation for a network"""
+
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
+def create_train_op(loss, alpha):
+    """loss is the loss of network's prediction
+    alpha is the learning rate
+    Returns: an operation that trains the network using
+    gradient descent"""
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train_op = optimizer.minize(loss)
+    return train_op
