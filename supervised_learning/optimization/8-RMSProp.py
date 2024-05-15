ec74e85a6e9b3c7aa778cf1d8bb14d370cb402ca
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-15 10:57:55 -0400

    ADD a function that Creates the RMSProp optimization operation in TensorFlow.

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -2,15 +2,16 @@
 """
 module create_RMSProp_op
 """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
+import tensorflow.keras as keras
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
+def create_RMSProp_op(alpha, beta2, epsilon):
     """
     creates the training operation for a neural network in tensorflow using
     the RMSProp optimization algorithm
     """
-    op = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2,
-                                   epsilon=epsilon)
-    step_op = op.minimize(loss)
+    op = tf.keras.optimizers.RMSprop(learning_rate=alpha, decay=beta2,
+                                     epsilon=epsilon)
+    step_op = op.get_updates(loss=None, params=None)
     return step_op
