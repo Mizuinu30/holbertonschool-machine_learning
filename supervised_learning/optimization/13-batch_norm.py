d7c09caf91ad44a979e0a5c999ef795d43aae6b3
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-16 20:06:13 -0400

    fixed pycode

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    Normalizes an unactivated output of a 
+    Normalizes an unactivated output of a
     neural network using batch normalization.
 
     Parameters:
