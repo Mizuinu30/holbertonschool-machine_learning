c6f2da007a358ac6f89a1eb54317551b2e2594d4
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-23 19:21:50 -0400

    calculates the cost of a neural network with L2 regularization

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -2,22 +2,11 @@
 """
 module l2_reg_cost
 """
-
 import tensorflow as tf
-import tensorflow.keras as K
 
 
 def l2_reg_cost(cost, model):
-    """
-    Calculates the cost of a neural network with L2 regularization.
-
-    Parameters:
-    cost (tensor): A tensor containing the cost of the network without L2 regularization.
-    model (Keras Model): A Keras model that includes layers with L2 regularization.
-
-    Returns:
-    tensor: A tensor containing the total cost for each layer of the network, accounting for L2 regularization.
-    """
-    l2_loss = sum(layer.losses for layer in model.layers)
-    total_cost = cost + l2_loss
+    """ calculates the cost of a neural network with L2 regularization"""
+    reg_losses = tf.add_n(model.losses)
+    total_cost = cost + reg_losses
     return total_cost
