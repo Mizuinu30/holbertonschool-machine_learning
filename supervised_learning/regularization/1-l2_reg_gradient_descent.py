1f9dd4ad4ecafcd16c87e619acd15c64d34b1683
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-23 20:57:09 -0400

    hot fix

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,24 +8,25 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """ updates the weights and biases of a neural
     network using gradient descent"""
+
     m = Y.shape[1]
     dZ = cache['A' + str(L)] - Y
 
-    for l in range(L, 0, -1):
-        A_prev = cache['A' + str(l - 1)] if l > 1 else cache['A0']
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(l - 1)] if i > 1 else cache['A0']
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
 
         # Calculate gradients
         dW = (np.dot(dZ, A_prev.T) / m) + (lambtha / m) * W
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
         # Update parameters
-        weights['W' + str(l)] = W - alpha * dW
-        weights['b' + str(l)] = b - alpha * db
+        weights['W' + str(i)] = W - alpha * dW
+        weights['b' + str(i)] = b - alpha * db
 
-        if l > 1:
+        if i > 1:
             # Calculate dZ for the next layer
-            dZ = np.dot(W.T, dZ) * (1 - cache['A' + str(l - 1)] ** 2)
+            dZ = np.dot(W.T, dZ) * (1 - cache['A' + str(i - 1)] ** 2)
 
     return None
