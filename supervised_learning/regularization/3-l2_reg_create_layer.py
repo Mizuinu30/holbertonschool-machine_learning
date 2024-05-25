f2450775e7978b75b3b196aed6e56f00ef436345
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-25 19:56:50 -0400

    Add the correct import

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -4,23 +4,14 @@
 """
 
 import tensorflow as tf
+from tensorflow.keras import layers, regularizers
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """
-    Function to create a tf layer with L2 regularization
-    Args:
-        prev: tensor containing the output of the previous layer
-        n: the number of nodes the new layer should contain
-        activation:  the activation function that should be used on the layer
-        lambtha: L2 regularization parameter
-
-    Returns: output of the new layer
-
-    """
-    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    tensor = tf.layers.Dense(units=n, activation=activation,
-                             kernel_initializer=init,
-                             kernel_regularizer=regularizer)
-    return tensor(prev)
+    """Function that creates a tf layer that"""
+    layer = layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_regularizer=regularizers.l2(lambtha)
+    )
+    return layer(prev)
