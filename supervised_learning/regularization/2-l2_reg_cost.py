32995bd24daeb3e0dba8582df23b21150dc1eb31
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-25 19:49:33 -0400

    hotfix

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -7,10 +7,18 @@ import tensorflow as tf
 def l2_reg_cost(cost, model):
     """ calculates the cost of a neural network with L2 regularization"""
 
-    l2_losses = [tf.reduce_sum(layer.losses)
-                 for layer in model.layers if layer.losses]
+    l2_losses = []
 
-    # Add the L2 losses to the original cost
-    total_cost = tf.constant([cost + l2_loss for l2_loss in l2_losses])
+    # Iterate over each layer to get L2 losses
+    for layer in model.layers:
+        if layer.losses:  # Check if the layer has L2 regularization losses
+            l2_loss = tf.reduce_sum(layer.losses)
+            l2_losses.append(l2_loss)
 
-    return total_cost
+    # Convert the list of L2 losses to a tensor
+    l2_losses_tensor = tf.convert_to_tensor(l2_losses)
+
+    # Calculate total costs by adding the original cost to each L2 loss
+    total_costs = cost + l2_losses_tensor
+
+    return total_costs
