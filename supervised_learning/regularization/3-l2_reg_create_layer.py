f470ebd6a1275acc192efd8e21dae115c06e67cf
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-25 20:14:44 -0400

    Add the correct import

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """ L2 Regularization Cost """
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras import layers, regularizers
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """ creates a tensorflow layer that includes L2 regularization
-        prev: tensor containing the output of the previous layer
-        n: number of nodes the new layer should contain
-        activation: activation function that should be used on the layer
-        lambtha: L2 regularization parameter
-        Returns: output of the new layer
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    reg = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(n, activation, kernel_initializer=init,
-                            kernel_regularizer=reg)
+    creates a tensorflow layer that includes L2 regularization
+    """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode='fan_avg', distribution='untruncated_normal')
+
+    layer = layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizers.l2(lambtha)
+    )
     return layer(prev)
