2bec86307fe970e9ee949c61b79c48ade6848e12
Mizuinu30 <juansilva.dvm@gmail.com>
2024-05-27 18:32:57 -0400

    init commit

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" a function that builds a neural network with the Keras library"""
+
+
+import tensorflow as tf
+import keras as K
+from keras import layers
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ a function that builds a neural network with the Keras library"""
+    model = K.Sequential()
+    L2 = K.regularizers.l2(lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(layers.Dense(layers[i], activation=activations[i],
+                                   kernel_regularizer=L2, input_shape=(nx,)))
+        else:
+            model.add(layers.Dropout(1 - keep_prob))
+            model.add(layers.Dense(layers[i], activation=activations[i],
+                                   kernel_regularizer=L2))
+    return model
