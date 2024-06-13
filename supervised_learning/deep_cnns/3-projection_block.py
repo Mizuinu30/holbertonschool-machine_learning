e33f62b4c641a175afd9d208f445610c6ddd2f55
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 20:53:27 -0400

    commented code

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -7,27 +7,46 @@ from tensorflow import keras as K
 
 def projection_block(A_prev, filters, s=2):
     """
-    builds a projection block as described in Deep Residual Learning for Image
-    Recognition(2015)
+    Builds a projection block as described in Deep Residual Learning for Image Recognition (2015)
+
+    Args:
+        A_prev (tensor): output from the previous layer
+        filters (tuple or list): contains F11, F3, F12
+            - F11 is the number of filters in the first 1x1 convolution
+            - F3 is the number of filters in the 3x3 convolution
+            - F12 is the number of filters in the second 1x1 convolution
+        s (int): stride to be used for the first convolution in both the main and shortcut paths
+
+    Returns:
+        tensor: the activated output of the projection block
     """
-    K.set_seed(0)
-    init = K.initializers.he_normal()
     F11, F3, F12 = filters
-    conv1 = K.layers.Conv2D(filters=F11, kernel_size=1, strides=s,
-                            padding='same', kernel_initializer=init)(A_prev)
-    batch1 = K.layers.BatchNormalization(axis=3)(conv1)
-    relu1 = K.layers.Activation('relu')(batch1)
-    conv2 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
-                            kernel_initializer=init)(relu1)
-    batch2 = K.layers.BatchNormalization(axis=3)(conv2)
-    relu2 = K.layers.Activation('relu')(batch2)
-    conv3 = K.layers.Conv2D(filters=F12, kernel_size=1, padding='same',
-                            kernel_initializer=init)(relu2)
-    conv1_proj = K.layers.Conv2D(filters=F12, kernel_size=1, strides=s,
-                                 padding='same',
-                                 kernel_initializer=init)(A_prev)
-    batch3 = K.layers.BatchNormalization(axis=3)(conv3)
-    batch4 = K.layers.BatchNormalization(axis=3)(conv1_proj)
-    add = K.layers.Add()([batch3, batch4])
-    final_relu = K.layers.Activation('relu')(add)
-    return final_relu
+    init = K.initializers.he_normal(seed=0)
+
+    # First component of the main path
+    X = K.layers.Conv2D(F11, (1, 1), strides=s, padding='same',
+                        kernel_initializer=init)(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Second component of the main path
+    X = K.layers.Conv2D(F3, (3, 3), strides=(
+        1, 1), padding='same', kernel_initializer=init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    # Third component of the main path
+    X = K.layers.Conv2D(F12, (1, 1), strides=(
+        1, 1), padding='same', kernel_initializer=init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+
+    # Shortcut path
+    X_shortcut = K.layers.Conv2D(
+        F12, (1, 1), strides=s, padding='same', kernel_initializer=init)(A_prev)
+    X_shortcut = K.layers.BatchNormalization(axis=3)(X_shortcut)
+
+    # Add shortcut to the main path
+    X = K.layers.Add()([X, X_shortcut])
+    X = K.layers.Activation('relu')(X)
+
+    return X
