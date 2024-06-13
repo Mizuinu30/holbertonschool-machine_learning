8d90423961405410e82a3fd74ed9f425a5e1c003
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 20:45:59 -0400

    commented code

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -4,8 +4,18 @@ from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
-    """Builds an identity block as described in
-    Deep Residual Learning for Image Recognition (2015)"""
+    """Builds an identity block as described in Deep Residual Learning for Image Recognition (2015)
+
+    Args:
+        A_prev (tensor): output from the previous layer
+        filters (tuple or list): contains F11, F3, F12
+            - F11 is the number of filters in the first 1x1 convolution
+            - F3 is the number of filters in the 3x3 convolution
+            - F12 is the number of filters in the second 1x1 convolution
+
+    Returns:
+        tensor: the activated output of the identity block
+    """
     F11, F3, F12 = filters
 
     # Save the input value for the shortcut
@@ -14,8 +24,7 @@ def identity_block(A_prev, filters):
     # First component of main path
     X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
                         strides=(1, 1), padding='valid',
-                        kernel_initializer=K.initializers.he_normal(seed=0)
-                        )(A_prev)
+                        kernel_initializer=K.initializers.he_normal(seed=0))(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
@@ -27,12 +36,12 @@ def identity_block(A_prev, filters):
     X = K.layers.Activation('relu')(X)
 
     # Third component of main path
-    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
-                        padding='valid',
+    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
+                        strides=(1, 1), padding='valid',
                         kernel_initializer=K.initializers.he_normal(seed=0))(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
-    # Add shortcut value to main path, and pass it through a RELU activation
+    # Add shortcut value to main path, and pass it through a ReLU activation
     X = K.layers.Add()([X, X_shortcut])
     X = K.layers.Activation('relu')(X)
 
