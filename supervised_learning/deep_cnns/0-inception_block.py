669de8e2c90db1933afa1cf351d4448b486f4a73
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 20:28:03 -0400

    fixed pycode

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -7,7 +7,8 @@ from tensorflow import keras as K
 
 def inception_block(A_prev, filters):
     """
-    Builds an inception block as described in Going Deeper with Convolutions (2014)
+    Builds an inception block as described in
+    Going Deeper with Convolutions (2014)
 
     Args:
         A_prev (ndarray): Output from the previous layer
@@ -22,25 +23,31 @@ def inception_block(A_prev, filters):
 
     # 1x1 Convolution branch
     conv_1x1 = K.layers.Conv2D(filters=F1, kernel_size=1, padding='same',
-                               activation=activation, kernel_initializer=init)(A_prev)
+                               activation=activation, kernel_initializer=init)(
+                                   A_prev)
 
     # 1x1 Convolution followed by 3x3 Convolution branch
     conv_3x3_reduce = K.layers.Conv2D(filters=F3R, kernel_size=1, padding='same',
-                                      activation=activation, kernel_initializer=init)(A_prev)
+                                      activation=activation, kernel_initializer=init)(
+                                          A_prev)
     conv_3x3 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
-                               activation=activation, kernel_initializer=init)(conv_3x3_reduce)
+                               activation=activation, kernel_initializer=init)(
+                                   conv_3x3_reduce)
 
     # 1x1 Convolution followed by 5x5 Convolution branch
     conv_5x5_reduce = K.layers.Conv2D(filters=F5R, kernel_size=1, padding='same',
-                                      activation=activation, kernel_initializer=init)(A_prev)
+                                      activation=activation, kernel_initializer=init)(
+                                          A_prev)
     conv_5x5 = K.layers.Conv2D(filters=F5, kernel_size=5, padding='same',
-                               activation=activation, kernel_initializer=init)(conv_5x5_reduce)
+                               activation=activation, kernel_initializer=init)(
+                                   conv_5x5_reduce)
 
     # Max pooling followed by 1x1 Convolution branch
     max_pool = K.layers.MaxPooling2D(pool_size=(
         3, 3), strides=(1, 1), padding='same')(A_prev)
     max_pool_conv = K.layers.Conv2D(filters=FPP, kernel_size=1, padding='same',
-                                    activation=activation, kernel_initializer=init)(max_pool)
+                                    activation=activation, kernel_initializer=init)(
+                                        max_pool)
 
     # Concatenate all the branches
     inception_output = K.layers.concatenate(
