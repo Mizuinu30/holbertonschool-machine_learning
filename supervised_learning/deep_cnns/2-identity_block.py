305e2c785dfd611688b0c8f6499ef4faa6f16eca
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 20:38:26 -0400

    builds an identity block as described in Deep Residual Learning for Image     Recognition(2015)

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """
-Deep Residual Learning Module
+Deep CNNs Module
 """
 from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
     """
-    Builds an identity block as described in Deep
-    Residual Learning for Image Recognition (2015)
+    Builds an identity block as described in Deep Residual Learning for Image Recognition (2015)
 
     Args:
         A_prev (tensor): output from the previous layer
@@ -24,19 +23,18 @@ def identity_block(A_prev, filters):
     init = K.initializers.he_normal(seed=0)
 
     # First component of the main path
-    X = K.layers.Conv2D(F11, (1, 1), strides=(1, 1), padding='valid',
+    X = K.layers.Conv2D(F11, (1, 1), padding='same',
                         kernel_initializer=init)(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
     # Second component of the main path
-    X = K.layers.Conv2D(F3, (3, 3), strides=(1, 1), padding='same',
-                        kernel_initializer=init)(X)
+    X = K.layers.Conv2D(F3, (3, 3), padding='same', kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
     # Third component of the main path
-    X = K.layers.Conv2D(F12, (1, 1), strides=(1, 1), padding='valid',
+    X = K.layers.Conv2D(F12, (1, 1), padding='same',
                         kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
