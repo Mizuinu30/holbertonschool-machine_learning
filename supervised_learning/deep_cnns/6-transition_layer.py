315f26654432c3e0f3188cc0a923db4530729f96
Mizuinu30 <juansilva.dvm@gmail.com>
2024-06-12 21:16:07 -0400

    Builds a transition layer

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -7,32 +7,17 @@ from tensorflow import keras as K
 
 def transition_layer(X, nb_filters, compression):
     """Builds a transition layer as described in Densely Connected
-    Convolutional Networks
-
-    Args:
-        X is the output from the previous layer
-        nb_filters is an integer representing the number of filters in X
-        compression is the compression factor for the transition layer
-
-    Returns:
-        The output of the transition layer and the number of filters within
-        the output, respectively
-    """
-    # He et. al initialization
-    init = K.initializers.he_normal()
-    # Batch Normalization
-    batch_norm = K.layers.BatchNormalization()(X)
-    # ReLU activation function
-    activation = K.layers.Activation('relu')(batch_norm)
+    Convolutional Networks"""
     # 1x1 Convolution
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
     nb_filters = int(nb_filters * compression)
-
-    conv = K.layers.Conv2D(filters=nb_filters,
-                           kernel_size=(1, 1),
-                           padding='same',
-                           kernel_initializer=init)(activation)
+    # 1x1 Convolution
+    X = K.layers.Conv2D(nb_filters, (1, 1),
+                        padding='same',
+                        kernel_initializer=K.initializers.he_normal(
+        seed=0))(X)
     # Average Pooling
-    avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2),
-                                         strides=(2, 2))(conv)
+    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2))(X)
     # Return the output of the transition layer and the number of filters
-    return avg_pool, nb_filters
+    return X, nb_filters
