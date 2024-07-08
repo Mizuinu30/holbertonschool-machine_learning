d3d34a9d7b687ccbe6f12b7a8488cf8b1864968f
Mizuinu30 <juansilva.dvm@gmail.com>
2024-07-07 21:10:04 -0400

    Fix pycode

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -30,7 +30,8 @@ class NST:
         if not isinstance(style_image, np.ndarray) or style_image.ndim != 3 \
                 or style_image.shape[-1] != 3:
             raise TypeError(error1)
-        if not isinstance(content_image, np.ndarray) or content_image.ndim != 3 \
+        if not isinstance(
+            content_image, np.ndarray) or content_image.ndim != 3 \
                 or content_image.shape[-1] != 3:
             raise TypeError(error2)
         if not (isinstance(alpha, (float, int)) and alpha >= 0):
@@ -77,12 +78,14 @@ class NST:
         )
         custom_object = {"MaxPooling2D": tf.keras.layers.AveragePooling2D}
         base_vgg.save("base_vgg")
-        vgg = tf.keras.models.load_model("base_vgg", custom_objects=custom_object)
+        vgg = tf.keras.models.load_model(
+            "base_vgg", custom_objects=custom_object)
 
         for layer in vgg.layers:
             layer.trainable = False
 
-        style_outputs = [vgg.get_layer(name).output for name in self.style_layers]
+        style_outputs = [vgg.get_layer(
+            name).output for name in self.style_layers]
         content_output = vgg.get_layer(self.content_layer).output
         outputs = style_outputs + [content_output]
         self.model = tf.keras.models.Model(inputs=vgg.input, outputs=outputs)
@@ -92,12 +95,14 @@ class NST:
         """
         Calculates the gram matrix of a layer
         Arguments:
-            input_layer {tf.Tensor} -- the layer for which to calculate the gram matrix
+            input_layer {tf.Tensor} -- the
+            layer for which to calculate the gram matrix
         Returns:
             tf.Tensor -- the gram matrix
         """
         error = "input_layer must be a tensor of rank 4"
-        if not isinstance(input_layer, (tf.Tensor, tf.Variable)) or len(input_layer.shape) != 4:
+        if not isinstance(input_layer, (
+            tf.Tensor, tf.Variable)) or len(input_layer.shape) != 4:
             raise TypeError(error)
 
         result = tf.linalg.einsum('bijc,bijd->bcd', input_layer, input_layer)
@@ -109,7 +114,8 @@ class NST:
         """
         Extracts features used to calculate Neural Style Transfer cost
         Sets the public instance attributes:
-            gram_style_features - a list of gram matrices calculated from the style layer outputs
+            gram_style_features - a list of
+            gram matrices calculated from the style layer outputs
             content_feature - the content later output of the content image
         """
         style_image = tf.keras.applications.vgg19.preprocess_input(
@@ -140,11 +146,13 @@ class NST:
         c = style_output.shape[-1]
 
         err_1 = "style_output must be a tensor of rank 4"
-        if not isinstance(style_output, (tf.Tensor, tf.Variable)) or len(style_output.shape) != 4:
+        if not isinstance(style_output, (
+            tf.Tensor, tf.Variable)) or len(style_output.shape) != 4:
             raise TypeError(err_1)
 
         err_2 = "gram_target must be a tensor of shape [1, {}, {}]".format(c, c)
-        if not isinstance(gram_target, (tf.Tensor, tf.Variable)) or gram_target.shape != (1, c, c):
+        if not isinstance(gram_target, (
+            tf.Tensor, tf.Variable)) or gram_target.shape != (1, c, c):
             raise TypeError(err_2)
 
         gram_style = self.gram_matrix(style_output)
@@ -156,19 +164,22 @@ class NST:
         """
         Calculates the style cost for generated image
         Arguments:
-            style_outputs {list} -- a list of tf.Tensor containing the style outputs for the generated image
+            style_outputs {list} -- a list of tf.Tensor
+              containing the style outputs for the generated image
         Returns:
             tf.Tensor -- the style cost
         """
         st_len = len(self.style_layers)
         err_list_check = "style_outputs must be a list with a length of {}".format(st_len)
-        if not isinstance(style_outputs, list) or len(self.style_layers) != len(style_outputs):
+        if not isinstance(style_outputs, list) or len(
+            self.style_layers) != len(style_outputs):
             raise TypeError(err_list_check)
 
         style_costs = []
         weight = 1 / len(self.style_layers)
 
-        for style_output, gram_target in zip(style_outputs, self.gram_style_features):
+        for style_output, gram_target in zip(
+            style_outputs, self.gram_style_features):
             layer_style_cost = self.layer_style_cost(style_output, gram_target)
             weighted_layer_style_cost = weight * layer_style_cost
             style_costs.append(weighted_layer_style_cost)
