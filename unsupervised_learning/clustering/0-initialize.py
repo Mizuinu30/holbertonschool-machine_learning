22f8fb64bb3ee0280f050cdbdea14d694db46a31
Mizuinu30 <juansilva.dvm@gmail.com>
2024-07-31 21:09:58 -0400

    fixed pycode

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -2,18 +2,21 @@
 
 import numpy as np
 
+
 def initialize(X, k):
     """
     Initializes cluster centroids for K-means.
 
     Parameters:
-    X (numpy.ndarray): The dataset of shape (n, d) to be used for K-means clustering.
+    X (numpy.ndarray): The dataset of shape (n, d) to
+    be used for K-means clustering.
         n is the number of data points.
         d is the number of dimensions for each data point.
     k (int): A positive integer containing the number of clusters.
 
     Returns:
-    numpy.ndarray: A numpy array of shape (k, d) containing the initialized centroids for each cluster,
+    numpy.ndarray: A numpy array of shape (k, d) containing
+    the initialized centroids for each cluster,
                    or None on failure.
     """
     if not isinstance(X, np.ndarray) or not isinstance(k, int) or k <= 0:
