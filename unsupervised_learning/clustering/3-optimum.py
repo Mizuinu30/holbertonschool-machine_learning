19ed497e18733054735649a4ed59b512871b8ebc
Mizuinu30 <juansilva.dvm@gmail.com>
2024-08-04 19:36:27 -0400

    fixed pycode

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,50 +1,45 @@
 #!/usr/bin/env python3
-"""tests for the optimum number of clusters by variance"""
-
-
+"""This modle  test fr the optimum number
+of clusters by variance
+"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """ tests for the optimum number of clusters by variance
-    @X: np.ndarray shape(n, d) data set
-    @kmin: pos int - min number of clusters to check for (inclusive)
-    @kmax: pos int - max number of clusters to check for (inclusive)
-    @iterations: pos int - max number of iterations for K-means
-    *should analyze atleast 2 diff cluster sizes
-    Returns: results, d_vars, or None, None on failure
-        @results: list containing the outputs of K-means for each cluster size
-        @d_vars: list - the diff in variance from smallest cluster size
-        for each cluster size
+    """This function tests fr the optimum number of clusters by variance
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None
+
+    n, d = X.shape
+
     if kmax is None:
-        kmax = X.shape[0]
-    if not isinstance(kmin, int) or not isinstance(kmax, int):
+        kmax = n
+    if not isinstance(kmin, int) or kmin <= 0 or n <= kmin:
         return None, None
-    if not isinstance(iterations, int):
+    if not isinstance(kmax, int) or kmax <= 0 or n < kmax:
         return None, None
-    if kmin < 1 or kmax < 1 or kmin >= kmax or iterations < 1:
+    if kmin >= kmax:
         return None, None
 
-    results, d_vars = [], []
-    first_var = None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
 
-    for i in range(kmin, kmax + 1):
-        centroids, clss = kmeans(X, i, iterations)
-        results.append((centroids, clss))
-        var = variance(X, centroids)
+    results = []
+    variances = []
+    d_vars = []
 
-        if var is None:
-            continue  # Skip this iteration if var is None
+    for k in range(kmin, kmax + 1):
 
-        if first_var is None:
-            first_var = var
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
 
-        # get diff
-        d_vars.append(first_var - var)
+        V = variance(X, C)
+        variances.append(V)
+
+    for var in variances:
+        d_vars.append(np.abs(variances[0] - var))
 
     return results, d_vars
