fe220c0f2923503937939e8d12bfa8dcbd2f8fd8
Mizuinu30 <juansilva.dvm@gmail.com>
2024-08-15 20:05:41 -0400

    Performs Bayesian optimization on a noiseless 1D Gaussian process

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,24 +1,53 @@
+#!/usr/bin/env python3
+"""This modlue contain the ByseianOptimization class
+    That erforms Byseian optimization on a noiseless
+    1D Gaussian process"""
 import numpy as np
-from 2-gp import GaussianProcess
+GP = __import__('2-gp').GaussianProcess
+
 
 class BayesianOptimization:
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
-        """
-        Initializes the BayesianOptimization class.
+    """This class performs Bayesian optimization on a
+    noiseless 1D Gaussian process """
 
-        Parameters:
-        - f: the black-box function to be optimized
-        - X_init: numpy.ndarray of shape (t, 1) representing the inputs already sampled with the black-box function
-        - Y_init: numpy.ndarray of shape (t, 1) representing the outputs of the black-box function for each input in X_init
-        - bounds: tuple of (min, max) representing the bounds of the space in which to look for the optimal point
-        - ac_samples: number of samples that should be analyzed during acquisition
-        - l: length parameter for the kernel
-        - sigma_f: standard deviation given to the output of the black-box function
-        - xsi: exploration-exploitation factor for acquisition
-        - minimize: bool determining whether optimization should be performed for minimization (True) or maximization (False)
+    def __init__(
+        self,
+        f,
+        X_init,
+        Y_init,
+        bounds,
+        ac_samples,
+        l=1,
+        sigma_f=1,
+        xsi=0.01,
+        minimize=True,
+    ):
+        """This method initializes the class BayesianOpt
+        Args:
+            f: the black-box function to be optimized
+            X_init: numpy.ndarray of shape (t, 1) representing the inputs
+                    already sampled with the black-box function
+            Y_init: numpy.ndarray of shape (t, 1) representing the outputs
+                    of the black-box function for each input in X_init
+            t: the number of initial samples
+            bounds: tuple of (min, max) representing the bounds of the space
+                    in which to look for the optimal point
+            ac_samples: the number of samples that should be analyzed during
+                        acquisition
+            l: the length parameter for the kernel
+            sigma_f: the standard deviation given to the output of the
+                    black-box function
+            xsi: the exploration-exploitation factor for acquisition
+            minimize: a bool determining whether optimization should be
+                    performed for minimization (True) or maximization (False)
         """
         self.f = f
-        self.gp = GaussianProcess(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        # gp is an instance of the GaussianProcess class
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(
+            bounds[0], bounds[1], num=ac_samples).reshape(-1, 1)
+        # print(f"-"*20)
+        # print(f"X_s: {self.X_s}")
+        # print(f"-"*20)
         self.xsi = xsi
         self.minimize = minimize
