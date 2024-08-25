5d3f679672b16149b0f5c4c6521f2e47c4a3d749
Mizuinu30 <juansilva.dvm@gmail.com>
2024-08-25 12:52:51 -0400

    Creates a variational autoencoder

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -1,60 +1,52 @@
 #!/usr/bin/env python3
-"""
-Defines function that creates a variational autoencoder
-"""
+"""This modlue contains a function that creates a variational autoencoder"""
+import tensorflow.keras as keras
 
-
-from tensorflow.keras.layers import Input, Dense, Lambda
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import binary_crossentropy
-import tensorflow.keras.backend as K
-import numpy as np
-
-def sampling(args):
-    """ Sampling function for variational autoencoder """
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
-
-def vae_loss(input_layer, output_layer, z_mean, z_log_var):
-    """ Loss function for variational autoencoder """
-    reconstruction_loss = binary_crossentropy(input_layer, output_layer)
-    reconstruction_loss *= input_layer.shape[1]
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    return K.mean(reconstruction_loss + kl_loss)
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
-    """  Creates a variational autoencoder"""
-    # Encoder
-    input_layer = Input(shape=(input_dims,))
-    x = input_layer
-    for nodes in hidden_layers:
-        x = Dense(nodes, activation='relu')(x)
-    z_mean = Dense(latent_dims)(x)
-    z_log_var = Dense(latent_dims)(x)
-    z = Lambda(sampling, output_shape=(latent_dims,))([z_mean, z_log_var])
+    """This function creates a variational autoencoder
+    """
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    for idx, units in enumerate(hidden_layers):
+        layer = keras.layers.Dense(units=units, activation="relu")
+        if idx == 0:
+            outputs = layer(encoder_inputs)
+        else:
+            outputs = layer(outputs)
+    layer = keras.layers.Dense(units=latent_dims)
+    mean = layer(outputs)
+    layer = keras.layers.Dense(units=latent_dims)
+    log_variation = layer(outputs)
 
-    encoder = Model(input_layer, [z, z_mean, z_log_var], name='encoder')
+    def sampling(args):
+        """This function samples from the mean and log variation
+        """
+        mean, log_variation = args
+        epsilon = keras.backend.random_normal(shape=keras.backend.shape(mean))
+        return mean + keras.backend.exp(log_variation * 0.5) * epsilon
 
-    # Decoder
-    latent_inputs = Input(shape=(latent_dims,))
-    x = latent_inputs
-    for nodes in reversed(hidden_layers):
-        x = Dense(nodes, activation='relu')(x)
-    output_layer = Dense(input_dims, activation='sigmoid')(x)
+    z = keras.layers.Lambda(sampling, output_shape=(latent_dims,))(
+        [mean, log_variation]
+    )
+    encoder = keras.models.Model(
+        inputs=encoder_inputs, outputs=[z, mean, log_variation]
+    )
 
-    decoder = Model(latent_inputs, output_layer, name='decoder')
+    decoder_inputs = keras.Input(shape=(latent_dims,))
+    for idx, units in enumerate(reversed(hidden_layers)):
+        layer = keras.layers.Dense(units=units, activation="relu")
+        if idx == 0:
+            outputs = layer(decoder_inputs)
+        else:
+            outputs = layer(outputs)
+    layer = keras.layers.Dense(units=input_dims, activation="sigmoid")
+    outputs = layer(outputs)
+    decoder = keras.models.Model(inputs=decoder_inputs, outputs=outputs)
 
-    # VAE
-    outputs = decoder(encoder(input_layer)[0])
-    auto = Model(input_layer, outputs, name='vae')
+    outputs = encoder(encoder_inputs)
+    outputs = decoder(outputs[0])
+    auto = keras.models.Model(inputs=encoder_inputs, outputs=outputs)
 
-    # Compile VAE
-    auto.compile(optimizer=Adam(), loss=lambda y_true, y_pred: vae_loss(input_layer, y_pred, z_mean, z_log_var))
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
 
     return encoder, decoder, auto
