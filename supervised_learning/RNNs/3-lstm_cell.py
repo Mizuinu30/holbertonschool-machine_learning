8a718f597d3cbf05fad1ee540e5fc25e84324236
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-08 18:05:43 -0400

    Performs forward propagation for one time step

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -1,123 +1,79 @@
 #!/usr/bin/env python3
-"""
-Defines the class LSTMCell that represents an LSTM unit
-"""
-
-
+"""This module contains the LSTMCell class"""
 import numpy as np
 
 
 class LSTMCell:
-    """
-    Represents a LSTM unit
+    """This class represents an LSTM unit"""
 
-    class constructor:
-        def __init__(self, i, h, o)
-
-    public instance attributes:
-        Wf: forget gate weights
-        bf: forget gate biases
-        Wu: update gate weights
-        bu: update gate biases
-        Wc: intermediate cell state weights
-        bc: intermediate cell state biases
-        Wo: output gate weights
-        bo: output gate biases
-        Wy: output weights
-        by: output biases
-
-    public instance methods:
-        def forward(self, h_prev, c_prev, x_t):
-            performs forward propagation for one time step
-    """
     def __init__(self, i, h, o):
+        """Class constructor
+        Args:
+            i: is the dimensionality of the data
+            h: is the dimensionality of the hidden state
+            o: is the dimensionality of the outputs
         """
-        Class constructor
 
-        parameters:
-            i: dimensionality of the data
-            h: dimensionality of the hidden state
-            o: dimensionality of the outputs
-
-        creates public instance attributes:
-            Wf: forget gate weights
-            bf: forget gate biases
-            Wu: update gate weights
-            bu: update gate biases
-            Wc: intermediate cell state weights
-            bc: intermediate cell state biases
-            Wo: output gate weights
-            bo: output gate biases
-            Wy: output weights
-            by: output biases
-
-        weights should be initialized using random normal distribution
-        weights will be used on the right side for matrix multiplication
-        biases should be initiliazed as zeros
-        """
+        # Initialize the weights and biases
+        self.Wf = np.random.normal(size=(i + h, h))
         self.bf = np.zeros((1, h))
+
+        self.Wu = np.random.normal(size=(i + h, h))
         self.bu = np.zeros((1, h))
+
+        self.Wc = np.random.normal(size=(i + h, h))
         self.bc = np.zeros((1, h))
+
+        self.Wo = np.random.normal(size=(i + h, h))
         self.bo = np.zeros((1, h))
-        self.by = np.zeros((1, o))
-        self.Wf = np.random.normal(size=(h + i, h))
-        self.Wu = np.random.normal(size=(h + i, h))
-        self.Wc = np.random.normal(size=(h + i, h))
-        self.Wo = np.random.normal(size=(h + i, h))
+
         self.Wy = np.random.normal(size=(h, o))
-
-    def softmax(self, x):
-        """
-        Performs the softmax function
-
-        parameters:
-            x: the value to perform softmax on to generate output of cell
-
-        return:
-            softmax of x
-        """
-        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-        softmax = e_x / e_x.sum(axis=1, keepdims=True)
-        return softmax
-
-    def sigmoid(self, x):
-        """
-        Performs the sigmoid function
-
-        parameters:
-            x: the value to perform sigmoid on
-
-        return:
-            sigmoid of x
-        """
-        sigmoid = 1 / (1 + np.exp(-x))
-        return sigmoid
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, c_prev, x_t):
+        """This function performs forward propagation for one time step
+        Args:
+            h_prev: numpy.ndarray of shape (m, h) containing the previous
+                    hidden state
+            c_prev: numpy.ndarray of shape (m, h) containing the previous
+                    cell state
+            x_t: numpy.ndarray of shape (m, i) that contains the data input
+                 for the cell
+        Returns: h_next, c_next, y
+                 h_next: the next hidden state
+                 c_next: the next cell state
+                 y: the output of the cell
         """
-        Performs forward propagation for one time step
 
-        parameters:
-            h_prev [numpy.ndarray of shape (m, h)]:
-                contains previous hidden state
-                m: the batch size for the data
-                h: dimensionality of hidden state
-            c_prev [numpy.ndarray of shape (m, h)]:
-                contains previous cell state
-                m: the batch size for the data
-                h: dimensionality of hidden state
-            x_t [numpy.ndarray of shape (m, i)]:
-                contains data input for the cell
-                m: the batch size for the data
-                i: dimensionality of the data
+        cell_input = np.concatenate((h_prev, x_t), axis=1)
 
-        output of the cell should use softmax activation function
+        f_gate = self.sigmoid(np.matmul(cell_input, self.Wf) + self.bf)
+        u_gate = self.sigmoid(np.matmul(cell_input, self.Wu) + self.bu)
 
-        returns:
-            h_next, c_next, y:
-            h_next: the next hidden state
-            c_next: the next cell state
-            y: the output of the cell
+        c_intermidiary = np.tanh(np.matmul(cell_input, self.Wc) + self.bc)
+
+        c_next = c_prev * f_gate + u_gate * c_intermidiary
+
+        o_gate = self.sigmoid(np.matmul(cell_input, self.Wo) + self.bo)
+
+        h_next = o_gate * np.tanh(c_next)
+
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
+
+        return h_next, c_next, y
+
+    def sigmoid(self, x):
+        """This method calculates the sigmoid function
+        Args:
+            x: numpy.ndarray
+        Returns: the sigmoid function of x
         """
-        concatenation = np.concatenate((h_prev, x_t), axis=1)
-        u_gate = self.sigmoid
+        return 1 / (1 + np.exp(-x))
+
+    def softmax(self, x):
+        """This method calculates the softmax function
+        Args:
+            x: numpy.ndarray
+        Returns: the softmax function of x
+        """
+        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
