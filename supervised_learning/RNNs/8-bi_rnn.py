194ed6c0c20352418dacd29eee965e6dc7a9f48b
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-08 18:17:18 -0400

    fix pycode

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -5,6 +5,7 @@ Defines function that performs forward propagation for bidirectional RNN
 
 import numpy as np
 
+
 def bi_rnn(bi_cell, X, h_0, h_t):
     """
     Performs forward propagation for bidirectional RNN
