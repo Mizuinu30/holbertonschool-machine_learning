e0694635fe4ecf939b2eb5ba6d85541a57d17d47
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-16 16:13:51 -0400

    Update the code to use the correct keyword argument

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -5,7 +5,11 @@ This module contains a function to create, build, and train a gensim Word2Vec mo
 
 from gensim.models import Word2Vec
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, cbow=True, epochs=5, seed=0, workers=1):
+
+def word2vec_model(
+    sentences, vector_size=100,
+    min_count=5, window=5, negative=5,
+    cbow=True, epochs=5, seed=0, workers=1):
     """
     Creates, builds, and trains a gensim Word2Vec model.
 
@@ -13,9 +17,11 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
     - sentences: list of sentences to be trained on
     - vector_size: dimensionality of the embedding layer
     - min_count: minimum number of occurrences of a word for use in training
-    - window: maximum distance between the current and predicted word within a sentence
+    - window: maximum distance between
+    the current and predicted word within a sentence
     - negative: size of negative sampling
-    - cbow: boolean to determine the training type; True is for CBOW; False is for Skip-gram
+    - cbow: boolean to determine
+    the training type; True is for CBOW; False is for Skip-gram
     - epochs: number of iterations to train over
     - seed: seed for the random number generator
     - workers: number of worker threads to train the model
@@ -25,6 +31,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
     """
 
     sg = 0 if cbow else 1  # 0 for CBOW, 1 for Skip-gram
+    """ Create the Word2Vec model """
 
     model = Word2Vec(
         sentences=sentences,
