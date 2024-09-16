7a3ca40a152c832501947c9539d29dc18e513558
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-16 15:40:52 -0400

    add a model level docstring

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+""" This module contains a function to create, build, and train a gensim Word2Vec model. """
+
+
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5,
                    cbow=True, epochs=5, seed=0, workers=1):
     """Creates, builds, and trains a Word2Vec model using gensim."""
