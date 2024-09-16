94c596adbb8b884f1faed353ae2e51b366b5c7cb
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-16 16:57:33 -0400

    add a model level docstring

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -10,21 +10,7 @@ from gensim.models import Word2Vec
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
-    word2vec using gensim
-    Args:
-        sentences: list of sentences to be trained on
-        size: dimensionality of the embedding layer
-        min_count: the minimum number of occurrences of a
-                    word for use in training
-        window: maximum distance between the current and
-                predicted word within a sentence
-        negative: the size of negative sampling
-        cbow: boolean to determine the training type; True
-              is for CBOW; False is for Skip-gram
-        iterations:  number of iterations to train over
-        seed: seed for the random number generator
-        workers: number of worker threads to train the model
-    Returns: the trained model
+    Creates, builds, and trains a gensim Word2Vec model.
     """
 
     sg = 0 if cbow else 1  # 0 for CBOW, 1 for Skip-gram
