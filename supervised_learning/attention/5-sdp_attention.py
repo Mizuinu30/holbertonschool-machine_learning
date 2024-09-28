ab8c3b8edf2fc067e512cb30a29798098481d152
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-28 19:23:42 -0400

    Add SDP Attention

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
-""" SDP Attention decoding"""
-
+""" sdp attention """
 
 import tensorflow as tf
 
 
-def sdp_attention(Q, K, V mask=None)
-""" SDP Attention decoding
+def sdp_attention(Q, K, V, mask=None):
+    """
 
-Args:
-    Q: tensor with it's last two dimensions are (..., seq_len_q, dk)
-        containing the query matrix
-    K: tensor with it's last two diensions are (..., seq_len_v, dk)
-        containing the key matrix
-    V: tensor with it's last two dimensions are (..., seq_len_v, dv)
-        containing the value matrix
-    mask: tensor with it's last two dimensions are (..., seq_len_q, seq_len_v)
-        containing the optional mask, or default None
-        output: output, weights
-
-    Returns: output, weights """
+    Args:
+        Q: tensor with its last two dimensions as (..., seq_len_q, dk)
+           containing the query matrix
+        K: tensor with its last two dimensions as (..., seq_len_v, dk)
+           containing the key matrix
+        V: tensor with its last two dimensions as (..., seq_len_v, dv)
+           containing the value matrix
+        mask: tensor that can be broadcast into (..., seq_len_q, seq_len_v)
+              containing the optional mask, or defaulted to None
+        output, weights
+    Returns:
+    """
 
     q = tf.matmul(Q, K, transpose_b=True)
     # scale q
     dk = tf.cast(tf.shape(K)[-1], tf.float32)
-    scaled = q / tf.math.sqrt(dk)
+    scaled_q = q / tf.math.sqrt(dk)
 
-    if mask is not None"
-        scaled += (mask * -1e9)
+    if mask is not None:
+        scaled_q += (mask * -1e9)
 
     weights = tf.nn.softmax(scaled_q, axis=-1)
 
