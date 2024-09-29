516b06559ebb191a279c5d434acb78d04a8f03f6
Mizuinu30 <juansilva.dvm@gmail.com>
2024-09-29 01:20:08 -0400

    fix pycode

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -50,10 +50,10 @@ class Encoder(tf.keras.layers.Layer):
                 "hidden must be an int representing hidden units")
         if not isinstance(input_vocab, int):
             raise TypeError(
-                "input_vocab must be an int representing the input vocabulary size")
+                "input_vocab must be an int representing  input vocabulary size")
         if not isinstance(max_seq_len, int):
             raise TypeError(
-                "max_seq_len must be an int representing the max sequence length")
+                "max_seq_len must be an int representing  max sequence length")
         if not isinstance(drop_rate, float):
             raise TypeError(
                 "drop_rate must be a float representing the dropout rate")
