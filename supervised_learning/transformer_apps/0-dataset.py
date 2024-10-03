2382add49072789c63ba468dfffd5bcfc48a5202
Mizuinu30 <juansilva.dvm@gmail.com>
2024-10-03 13:12:40 -0400

    iunitial commit

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,36 +1,8 @@
-#!/usr/bin/env python3
-"""
-Defines class Dataset that loads and preps a dataset for machine translation
-"""
-
-
-import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
-
+import tensorflow as tf
+from tokenizers import BertWordPieceTokenizer
 
 class Dataset:
-    """
-    Loads and preps a dataset for machine translation
-
-    class constructor:
-        def __init__(self)
-
-    public instance attributes:
-        data_train:
-            contains the ted_hrlr_translate/pt_to_en
-                tf.data.Dataset train split, loaded as_supervided
-        data_valid:
-            contains the ted_hrlr_translate/pt_to_en
-                tf.data.Dataset validate split, loaded as_supervided
-        tokenizer_pt:
-            the Portuguese tokenizer created from the training set
-        tokenizer_en:
-            the English tokenizer created from the training set
-
-    instance method:
-        def tokenize_dataset(self, data):
-            that creates sub-word tokenizers for our dataset
-    """
     def __init__(self):
         """
         Class constructor
@@ -38,10 +10,10 @@ class Dataset:
         Sets the public instance attributes:
             data_train:
                 contains the ted_hrlr_translate/pt_to_en
-                    tf.data.Dataset train split, loaded as_supervided
+                    tf.data.Dataset train split, loaded as_supervised
             data_valid:
                 contains the ted_hrlr_translate/pt_to_en
-                    tf.data.Dataset validate split, loaded as_supervided
+                    tf.data.Dataset validate split, loaded as_supervised
             tokenizer_pt:
                 the Portuguese tokenizer created from the training set
             tokenizer_en:
@@ -53,30 +25,30 @@ class Dataset:
         self.data_valid = tfds.load("ted_hrlr_translate/pt_to_en",
                                     split="validation",
                                     as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """
-        Creates sub_word tokenizers for our dataset
+        Creates sub-word tokenizers for our dataset
 
-        parameters:
-            data [tf.data.Dataset]:
-                dataset to use whose examples are formatted as tuple (pt, en)
-                pt [tf.Tensor]:
-                    contains the Portuguese sentence
-                en [tf.Tensor]:
-                    contains the corresponding English sentence
-        returns:
-            tokenizer_pt, tokenizer_en:
-                tokenizer_pt: the Portuguese tokenizer
-                tokenizer_en: the English tokenizer
+        Args:
+            data: tf.data.Dataset whose examples are formatted as a tuple (pt, en)
+                pt is the tf.Tensor containing the Portuguese sentence
+                en is the tf.Tensor containing the corresponding English sentence
+
+        Returns:
+            tokenizer_pt: the Portuguese tokenizer
+            tokenizer_en: the English tokenizer
         """
-        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
-        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data),
-            target_vocab_size=(2 ** 15))
-        tokenizer_en = SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data),
-            target_vocab_size=(2 ** 15))
+        # Initialize the tokenizers
+        tokenizer_pt = BertWordPieceTokenizer("neuralmind/bert-base-portuguese-cased-vocab.txt", lowercase=False)
+        tokenizer_en = BertWordPieceTokenizer("bert-base-uncased-vocab.txt", lowercase=True)
+
+        # Train the tokenizers with a maximum vocabulary size of 2**13
+        pt_sentences = [pt.numpy().decode('utf-8') for pt, en in data]
+        en_sentences = [en.numpy().decode('utf-8') for pt, en in data]
+
+        tokenizer_pt.train_from_iterator(pt_sentences, vocab_size=2**13)
+        tokenizer_en.train_from_iterator(en_sentences, vocab_size=2**13)
+
         return tokenizer_pt, tokenizer_en
