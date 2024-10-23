8ad61ab7400924ecf593719c6a2f21083dbfe68f
Mizuinu30 <juansilva.dvm@gmail.com>
2024-10-22 20:29:37 -0400

    fix pycode

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -11,7 +11,7 @@ import gymnasium as gym
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """ loads pre-made FrozenLakeEnv environment from OpenAI's gym"""
     ENV = gym.make(
-        'FrozenLake-v1', desc=desc, map_name=map_name, is_slippery=is_slippery,
-          render_mode="ansi")
+        'FrozenLake-v1', desc=desc, map_name=map_name,
+           is_slippery=is_slippery, render_mode="ansi")
 
     return ENV
