5380f7b52803313cc5a2bc50f3a5f370042296e9
Mizuinu30 <juansilva.dvm@gmail.com>
2024-12-05 18:21:10 -0400

    This module taht analyzes data on a DF

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module analyzes data in a DataFrame
+"""
+
+
+def analyze(df):
+    """ This function analyzes data in a DataFrame """
+
+    if "Timestamp" in df.columns:
+            df = df.drop(columns=["Timestamp"])
+
+    return df.describe()
