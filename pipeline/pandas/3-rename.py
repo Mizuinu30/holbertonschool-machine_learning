7c772c216ad8ca0090af302cc6a94a0f9d4e8fa4
Mizuinu30 <juansilva.dvm@gmail.com>
2024-12-05 15:36:13 -0400

    Updates df

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -6,7 +6,9 @@ New code updates Pandas DataFrame script to:
 - display only the Datetime and Close columns
 """
 
+
 import pandas as pd
+
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
