e37cc26c93ff3d2519e4fcf785adf96de361e1da
Mizuinu30 <juansilva.dvm@gmail.com>
2024-12-07 21:06:49 -0400

    CORRECTED LINE 5 TO JUST SAY TITLE

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- uses hbtn_0d_tvshows database
 -- database name will be passed as an argument of the mysql command
 SELECT
-    tv_shows.title AS show_title,
+    tv_shows.title AS title,
     tv_show_genres.genre_id AS genre_id
 FROM
     tv_shows
