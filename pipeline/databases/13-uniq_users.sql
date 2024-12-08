a2607983a1da83b9b2e25d95cc73f17342964110
root <root@ThousandSunny>
2024-12-08 16:38:52 -0400

    created trable

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,6 +1,5 @@
 --- Creates a table called users
 --- If users already exists, script does not fail
-
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
