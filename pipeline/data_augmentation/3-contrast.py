bfbb71456de5bdef2fbabf1c291430d059a7d7b2
Mizuinu30 <juansilva.dvm@gmail.com>
2024-12-11 20:30:50 -0400

    A function to change the contrast of an image

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 """
-    Defines function that changes the contrast of an image
+Defines function that changes the contrast of an image
 """
-
 
 import tensorflow as tf
 
-
 def change_contrast(image, lower, upper):
-    """ Changes the contrast of an image
+    """
+    Changes the contrast of an image
 
     Args:
-        image [3D td.Tensor]:
-            contains the image to change contrast
-        lower [float]:
-            lower bound of the contrast range
-        upper [float]:
-            upper bound of the contrast range
+        image [3D tf.Tensor]: contains the image to change contrast
+        lower [float]: lower bound of the contrast range
+        upper [float]: upper bound of the contrast range
+
     Returns:
         the adjusted image
     """
-    return (tf.image.adjust_contrast(image, lower, upper))
+    contrast_factor = tf.random.uniform([], lower, upper)
+    return tf.image.adjust_contrast(image, contrast_factor)
